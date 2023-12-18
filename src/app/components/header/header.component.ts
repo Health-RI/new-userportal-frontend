bd8a82ad4d092b962909b1d9c8a1c4940fb380db
Kacem Bechka <kacem198@yahoo.fr>
2023-12-18 16:08:42 +0100

    18/make header and footer responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  faBars = faBars;
+
+  toggleDropdown(): void {
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    if (dropdownMenu) {
+      dropdownMenu.classList.toggle('hidden');
+    }
+  }
 }

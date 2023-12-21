c2b40d7cd6c7049c6825515ff70bdb0a0b63eadb
Bruno Pacheco <brunopacheco1@yahoo.com>
2023-12-21 14:08:32 +0100

    Get environment variables at runtime

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    backendUrl: "",
-    identityServerUrl: "",  // KeyCloak url + REALM 
-    identityServerClientId: 'gdi-userportal-app' // Replace with your client ID
+    backendUrl: process.env["BACKEND_URL"] || "",
+    identityServerUrl: process.env["IDENTITY_SERVER_URL"] || "",  // KeyCloak url + REALM 
+    identityServerClientId: process.env["IDENTITY_SERVER_CLIENT_ID"] || "gdi-userportal-app" // Replace with your client ID
 }

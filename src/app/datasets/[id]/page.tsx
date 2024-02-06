dc4765085887b051dd3dc8a007048523a9021dd4
Younès Adem <younes.adem@lnds.lu>
2024-02-06 13:43:32 +0100

    Revert "add routing and project structure"
    This reverts commit 3000b0097725d9507655b5da260c944ac678dc9f.

--- src/app/datasets/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-function DatasetDetailsPage({ params }: { params: { id: string } }) {
-  return <h1>Dataset {params.id}</h1>;
-}
-
-export default DatasetDetailsPage;
--- src/app/datasets/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function DatasetPage() {
-  return <h1>Datasets</h1>;
-}

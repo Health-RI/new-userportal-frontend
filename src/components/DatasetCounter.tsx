a3400ba0b2c019f650d68539e2341ef647ee37ab
Emiliana <emiliana.pali@lnds.lu>
2024-02-29 13:48:20 +0100

    fix: #57 remove loading

--- src/components/DatasetCounter.tsx
+++ src/components/DatasetCounter.tsx
@@ -9,16 +9,8 @@ export async function DatasetCounter() {
   const count = await datasetCount();
   return (
     <div className="mb-4 mt-10 flex items-baseline">
-      {count !== null ? (
-        <>
-          <p className="mr-3 text-4xl font-bold">
-            {count.toLocaleString("en-GB")}
-          </p>
-          <p className="self-end text-xl font-bold">Datasets</p>
-        </>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <p className="mr-3 text-4xl font-bold">{count.toLocaleString("en-GB")}</p>
+      <p className="self-end text-xl font-bold">Datasets</p>
     </div>
   );
 }

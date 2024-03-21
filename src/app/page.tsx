09e57448cd82b7c015fb02da5e3f10efd7d53300
Hans-Chrstian <hanschristian.vanderwerf@health-ri.nl>
2024-03-21 13:46:58 +0100

    fix merge conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,9 @@ import { DatasetCounter } from "@/components/DatasetCounter";
 import { PortalStatistics } from "@/components/PortalStatistics";
 import SearchBar from "@/components/SearchBar";
 import Image from "next/image";
-import logo from "../public/gdi-logo.png";
 import office from "../public/office.jpg";
 import dna from "../public/dna.jpg";
-import gdi_visuals from "../public/gdi-visuals.png";
+import hri_banner from "../public/HRI_banner.jpg";
 import Link from "next/link";
 
 const HomePage = () => {
@@ -27,29 +26,31 @@ const HomePage = () => {
           <SearchBar queryParams={initialQueryParams} size="large" />
           <div className="mx-auto mt-12 w-full rounded-lg bg-white transition-shadow duration-300 ease-in-out hover:shadow-sm lg:mx-0 lg:w-1/2">
             <h2 className="mb-4 text-4xl font-bold text-primary">
-              WELCOME TO GDI
+              Welkom bij Health-RI
             </h2>
             <p className="mb-6 text-lg">
-              The Genomic Data Infrastructure (GDI) project is enabling access
-              to genomic and related phenotypic and clinical data across Europe.
+              Betere gezondheid voor burgers en patiënten door hergebruik van
+              gezondheidsdata met een geïntegreerde
+              gezondheidsdata-infrastructuur voor onderzoek, beleid en
+              innovatie.
             </p>
             <div className="flex justify-center lg:justify-start">
               <Link href="/datasets">
                 <button className="inline-block rounded bg-primary px-4 py-2 font-bold text-white transition-colors duration-300 hover:bg-secondary">
-                  Discover GDI datasets
+                  Ontdek datasets
                 </button>
               </Link>
             </div>
           </div>
         </div>
         <div className="flex justify-center md:justify-end">
-          <div className="w-full max-w-[600px]">
+          <div className="w-full">
             <Image
-              src={gdi_visuals}
-              alt="GDI Visualization"
+              src={hri_banner}
+              alt="Health-RI Visualization"
+              className="rounded-lg"
               width={600}
               height={400}
-              className="rounded-lg"
             />
           </div>
         </div>
@@ -102,48 +103,32 @@ const HomePage = () => {
       <div className="mb-36 rounded-xl border bg-primary p-8 text-white">
         <div className="container mx-auto flex flex-wrap items-center justify-between md:flex-nowrap">
           <h2 className="mb-6 text-2xl font-bold text-warning md:mb-0 md:text-3xl lg:text-4xl">
-            PARTICIPATE
+            DOE MEE
           </h2>
           <div className="flex flex-1 flex-col items-center justify-center text-center">
             <div className="flex w-full flex-wrap justify-around text-warning">
               <a
-                href="https://b1mg-project.eu/"
+                href="https://www.health-ri.nl/
+                "
                 target="_blank"
                 rel="noopener noreferrer"
                 className="mr-2 rounded border border-warning px-4 py-2 transition duration-300 ease-in-out md:mr-4 md:px-6 md:py-3 lg:mr-5"
               >
-                Learn more about B1MG
+                Kom meer te weten over Health-RI
               </a>
               <a
-                href="https://gdi.onemilliongenomes.eu/"
+                href="/subscribe-newsletter"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="rounded border border-warning px-4 py-2 transition duration-300 ease-in-out md:px-6 md:py-3"
               >
-                Learn more about GDI
+                Meld je aan voor de nieuwsbrief
               </a>
             </div>
           </div>
         </div>
       </div>
-
-      <div className="my-12 flex flex-col items-center justify-between md:flex-row">
-        <div className="p-4 md:flex-1">
-          <h3 className="mb-4 text-3xl font-bold leading-tight text-primary md:text-4xl">
-            Realising a vision for European healthcare
-          </h3>
-          <p className="text-lg text-gray-700">
-            The GDI project brings together experts in life science, medicine,
-            computer science, ethics, and law.
-          </p>
-        </div>
-        <div className="flex justify-center p-4 md:flex-1 md:justify-end">
-          <div className="flex h-64 w-64 transform items-center justify-center overflow-hidden bg-white transition-transform duration-500 ease-in-out hover:scale-110">
-            <Image src={logo} alt="GDI Logo" />
-          </div>
-        </div>
-      </div>
-    </div>
+</div>
   );
 };
 

a05a049aa36d0845faf4180901d443834dc2f829
Younès Adem <younes.adem@lnds.lu>
2024-03-29 18:06:24 +0100

    feat: file upload in browser

--- src/app/applications/[id]/page.tsx
+++ src/app/applications/[id]/page.tsx
@@ -1,12 +1,14 @@
 // SPDX-FileCopyrightText: 2024 PNED G.I.E.
 //
 // SPDX-License-Identifier: Apache-2.0
+"use client";
 
 import PageHeading from "@/components/PageHeading";
 import Sidebar from "@/components/Sidebar";
 import Button from "@/components/button";
 import FieldAttachmentContainer from "@/components/fieldAttachmentContainer";
 import { faPaperPlane, faSave } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 import { createApplicationSidebarItems } from "./sidebarItems";
 
 type ApplicationDetailsPageProps = {
@@ -16,11 +18,20 @@ type ApplicationDetailsPageProps = {
 export default function ApplicationDetailsPage({
   params,
 }: ApplicationDetailsPageProps) {
+  const [files, setFiles] = useState<File[]>([]);
   const { id } = params;
   const sidebarItems = createApplicationSidebarItems();
+
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   return (
     <div className="mx-8 mt-20 md:mx-auto md:w-3/4 xl:mx-0 xl:grid xl:w-full xl:grid-cols-12 xl:gap-x-20">
-      <div className="col-span-8 col-start-3 xl:col-span-6 xl:col-start-3">
+      <form
+        onSubmit={onSubmit}
+        className="col-span-8 col-start-3 xl:col-span-6 xl:col-start-3"
+      >
         <div className="px-3">
           <div className="sm:flex sm:justify-between">
             <PageHeading>Application {id}</PageHeading>
@@ -29,6 +40,7 @@ export default function ApplicationDetailsPage({
                 type="warning"
                 text="Save changes"
                 icon={faSave}
+                disabled={files.length === 0}
                 className="h-fit text-[10px] sm:text-xs"
               />
               <Button
@@ -36,6 +48,7 @@ export default function ApplicationDetailsPage({
                 text="Submit"
                 className="h-fit text-[10px] sm:text-xs"
                 icon={faPaperPlane}
+                disabled={files.length === 0}
               />
             </div>
           </div>
@@ -50,10 +63,12 @@ export default function ApplicationDetailsPage({
 
         <div className="mt-5 h-[2px] bg-secondary opacity-80 xl:hidden"></div>
 
-        <FieldAttachmentContainer fieldName="Field 1" />
-        <FieldAttachmentContainer fieldName="Field 2" />
-        <FieldAttachmentContainer fieldName="Field 3" />
-      </div>
+        <FieldAttachmentContainer
+          fieldName="Field 1"
+          files={files}
+          setFiles={setFiles}
+        />
+      </form>
 
       <aside className="col-span-3 col-start-9 hidden xl:block">
         <Sidebar items={sidebarItems} />
--- src/components/fileUploaded.tsx
+++ src/components/fileUploaded.tsx
@@ -5,7 +5,17 @@
 import { faClose, faFileCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function FileUploaded() {
+type FileUploadedProps = {
+  fileId: number;
+  fileName: string;
+  files: File[];
+  setFiles: React.Dispatch<React.SetStateAction<File[]>>;
+};
+
+function FileUploaded({ fileId, fileName, setFiles }: FileUploadedProps) {
+  function onDeleteFile() {
+    setFiles((files) => files.filter((_, index) => index !== fileId));
+  }
   return (
     <div className="relative mt-5 flex items-center justify-between gap-x-1 rounded border-2 bg-white-smoke px-3 py-1.5 sm:gap-x-3">
       <div className="flex items-center gap-x-2 sm:gap-x-4">
@@ -13,9 +23,13 @@ function FileUploaded() {
           icon={faFileCircleCheck}
           className="text-base text-info"
         />
-        <h3 className="text-sm text-info">File1.pdf</h3>
+        <h3 className="text-sm text-info">{fileName}</h3>
       </div>
-      <FontAwesomeIcon icon={faClose} className="border-1 text-sm text-info" />
+      <FontAwesomeIcon
+        icon={faClose}
+        onClick={onDeleteFile}
+        className="border-1 cursor-pointer text-sm text-info"
+      />
     </div>
   );
 }

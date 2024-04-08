7023e4e73ddf8eeb821e08dec5938a7f21d4f8ff
Inderpal Singh <inderpal.singh@lnds.lu>
2024-04-08 09:53:15 +0200

    fix: spacing and filter icon

--- src/app/datasets/clientWrapper.tsx
+++ src/app/datasets/clientWrapper.tsx
@@ -9,10 +9,12 @@ import DatasetList from "@/components/datasetList";
 import FilterList from "@/components/filterList";
 import PaginationContainer from "@/components/paginationContainer";
 import SearchBar from "@/components/SearchBar";
+import PageContainer from "@/components/PageContainer";
 import { useWindowSize } from "@/hooks";
 import { Facet } from "@/services/ckan/types/packageSearch.types";
 import { PackageSearchResult } from "@/services/ckan/types/packageSearch.types";
 import { SCREEN_SIZE, pixelWidthToScreenSize } from "@/utils/windowSize";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter, faX } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 
@@ -38,56 +40,58 @@ export default function ClientWrapper({
   }, [screenSize]);
 
   return (
-    <div className="mt-10 grid grid-cols-12 gap-x-12 gap-y-7">
-      {isFilterOpen ? (
-        <div className="relative col-span-10 col-start-2 rounded-lg border bg-white-smoke p-6">
-          <FilterList
-            facets={facets}
-            displayContinueButton={true}
-            setIsFilterOpen={setIsFilterOpen}
-            queryParams={queryParams}
-          />
-          <Button
-            icon={faX}
-            className="absolute right-0 top-0 w-fit hover:bg-primary hover:text-white"
-            text=""
-            onClick={() => setIsFilterOpen(false)}
-          />
-        </div>
-      ) : (
-        <>
-          <div className="relative col-span-6 col-start-4 mb-12 mt-10 md:col-span-4 md:col-start-5">
-            <SearchBar queryParams={queryParams} />
-            <p className="mt-5 text-center text-sm text-info">
-              {`${datasets.count} ${datasets.count > 1 ? "datasets" : "dataset"} found`}
-            </p>
-            <Button
-              icon={faFilter}
-              className="absolute -right-16 top-1 h-10 w-fit bg-white-smoke text-xs text-info hover:border-info md:text-xs xl:hidden"
-              text=""
-              onClick={() => setIsFilterOpen(!isFilterOpen)}
-            />
-          </div>
-          <div className="border-1 col-start-2 col-end-5 hidden rounded-lg border bg-white-smoke p-6 xl:block">
+    <PageContainer>
+      <div className="grid grid-cols-12">
+        {isFilterOpen ? (
+          <div className="col-start-0 relative col-span-12 rounded-lg border bg-white-smoke">
             <FilterList
               facets={facets}
+              displayContinueButton={true}
               setIsFilterOpen={setIsFilterOpen}
               queryParams={queryParams}
             />
-          </div>
-          <div className="col-span-8 col-start-3 xl:col-span-6 xl:col-start-5">
-            <DatasetList datasets={datasets.datasets} />
-          </div>
-          <div className="col-span-4 col-start-5 mt-20 text-info">
-            <PaginationContainer
-              datasetCount={datasets.count}
-              datasetPerPage={datasetPerPage}
-              pathname="/datasets"
-              queryParams={queryParams}
+            <Button
+              icon={faX}
+              className="absolute right-0 top-0 w-fit hover:bg-primary hover:text-white"
+              text=""
+              onClick={() => setIsFilterOpen(false)}
             />
           </div>
-        </>
-      )}
-    </div>
+        ) : (
+          <>
+            <div className="col-start-0 col-span-12 flex items-center justify-between xl:col-span-10 xl:col-start-2">
+              <SearchBar queryParams={queryParams} />
+              <button
+                className="ml-4 h-11 rounded-lg bg-info px-4 text-xs text-white hover:bg-secondary md:text-xs xl:hidden"
+                onClick={() => setIsFilterOpen(!isFilterOpen)}
+              >
+                <FontAwesomeIcon icon={faFilter} />
+              </button>
+            </div>
+            <p className="col-start-0 col-span-12 mb-12 mt-5 text-center text-sm text-info">
+              {`${datasets.count} ${datasets.count > 1 ? "datasets" : "dataset"} found`}
+            </p>
+            <div className="border-1 col-start-0 col-span-4 mr-6 hidden rounded-lg border bg-white-smoke xl:block">
+              <FilterList
+                facets={facets}
+                setIsFilterOpen={setIsFilterOpen}
+                queryParams={queryParams}
+              />
+            </div>
+            <div className="col-start-0 col-span-12 xl:col-span-8 xl:col-start-5">
+              <DatasetList datasets={datasets.datasets} />
+            </div>
+            <div className="col-start-0 col-span-12 mt-20 text-info xl:col-span-8 xl:col-start-5">
+              <PaginationContainer
+                datasetCount={datasets.count}
+                datasetPerPage={datasetPerPage}
+                pathname="/datasets"
+                queryParams={queryParams}
+              />
+            </div>
+          </>
+        )}
+      </div>
+    </PageContainer>
   );
 }

01877c6b54c15a9a85f9dc2ecab3abe0b5df5330
Kacem Bechka <kacem198@yahoo.fr>
2024-04-17 14:53:15 +0200

    fix: #191 blocked eval scr issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,10 @@ export default function RootLayout({
       <head>
         <title>GDI - User Portal</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="default-src * 'self' data: 'unsafe-inline' 'unsafe-eval' *"
+        />
         <PublicEnvScript />
       </head>
       <body>

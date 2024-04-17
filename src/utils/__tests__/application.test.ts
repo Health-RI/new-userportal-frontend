6ee51728ff5cc1d477281b32562bfd84197410b8
Younès Adem <younes.adem@lnds.lu>
2024-04-17 11:10:25 +0200

    refactor: tests to make code compliant with Sonar

--- src/utils/__tests__/application.test.ts
+++ src/utils/__tests__/application.test.ts
@@ -8,256 +8,47 @@ import { addAttachmentIdToFieldValue, deleteAttachmentIdFromFieldValue, updateFo
 describe('Update application correctly when adding an attachment', () => {
   it('should concatenate the current field value with the attachment id when field value already contains values', () => {
     const forms: Form[] = getForms();
-
     const formId = 1;
     const fieldId = 2;
     const newAttachmentId = 193;
 
     const updatedForms = updateFormWithNewAttachment(forms, formId, fieldId, newAttachmentId, addAttachmentIdToFieldValue);
 
-    expect(updatedForms).toEqual([
-      {
-        id: 1,
-        internalName: 'form1',
-        externalTitle: [],
-        fields: [
-          {
-            id: 1,
-            value: '',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-          {
-            id: 2,
-            value: '4,5,193',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-        ],
-      },
-      {
-        id: 2,
-        internalName: 'form2',
-        externalTitle: [],
-        fields: [
-          {
-            id: 1,
-            value: '22',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-          {
-            id: 2,
-            value: '8,2',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-        ],
-      },
-    ]);
+    expect(updatedForms[0].fields[1].value).toEqual('4,5,193');
   });
 
   it('should set the field value as attachment id when field value is empty string', () => {
     const forms: Form[] = getForms();
-
     const formId = 1;
     const fieldId = 1;
     const newAttachmentId = 11;
-
     const updatedForms = updateFormWithNewAttachment(forms, formId, fieldId, newAttachmentId, addAttachmentIdToFieldValue);
 
-    expect(updatedForms).toEqual([
-      {
-        id: 1,
-        internalName: 'form1',
-        externalTitle: [],
-        fields: [
-          {
-            id: 1,
-            value: '11',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-          {
-            id: 2,
-            value: '4,5',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-        ],
-      },
-      {
-        id: 2,
-        internalName: 'form2',
-        externalTitle: [],
-        fields: [
-          {
-            id: 1,
-            value: '22',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-          {
-            id: 2,
-            value: '8,2',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-        ],
-      },
-    ]);
+    expect(updatedForms[0].fields[0].value).toEqual('11');
   });
 });
 
 describe('Update application correctly when removing an attachment', () => {
   it('should remove the attachment id from field value when field value already contains values', () => {
     const forms: Form[] = getForms();
-
     const formId = 2;
     const fieldId = 2;
     const attachmentId = 8;
 
     const updatedForms = updateFormWithNewAttachment(forms, formId, fieldId, attachmentId, deleteAttachmentIdFromFieldValue);
 
-    expect(updatedForms).toEqual([
-      {
-        id: 1,
-        internalName: 'form1',
-        externalTitle: [],
-        fields: [
-          {
-            id: 1,
-            value: '',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-          {
-            id: 2,
-            value: '4,5',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-        ],
-      },
-      {
-        id: 2,
-        internalName: 'form2',
-        externalTitle: [],
-        fields: [
-          {
-            id: 1,
-            value: '22',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-          {
-            id: 2,
-            value: '2',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-        ],
-      },
-    ]);
+    expect(updatedForms[1].fields[1].value).toEqual('2');
   });
 
   it('should set the field value as empty string when field value equals attachment id', () => {
     const forms: Form[] = getForms();
-
     const formId = 2;
     const fieldId = 1;
     const attachmentId = 22;
 
     const updatedForms = updateFormWithNewAttachment(forms, formId, fieldId, attachmentId, deleteAttachmentIdFromFieldValue);
 
-    expect(updatedForms).toEqual([
-      {
-        id: 1,
-        internalName: 'form1',
-        externalTitle: [],
-        fields: [
-          {
-            id: 1,
-            value: '',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-          {
-            id: 2,
-            value: '4,5',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-        ],
-      },
-      {
-        id: 2,
-        internalName: 'form2',
-        externalTitle: [],
-        fields: [
-          {
-            id: 1,
-            value: '',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-          {
-            id: 2,
-            value: '8,2',
-            optional: false,
-            private: false,
-            visible: false,
-            title: [],
-            type: '',
-          },
-        ],
-      },
-    ]);
+    expect(updatedForms[1].fields[0].value).toEqual('');
   });
 });
 

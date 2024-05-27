d4f19cec9417484eb1b53dbad302c5f633abfb65
Hans-Chrstian <hanschristian.vanderwerf@health-ri.nl>
2024-05-27 15:08:28 +0200

    Add som e text to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -47,30 +47,6 @@ const AboutPage: React.FC = () => {
           GitHub Issues
         </a>
         .
-        {/* <PageSubHeading className="my-4">Key Objectives</PageSubHeading>
-      <ul className="list-inside list-disc">
-        <li>
-          To link and to provide cross-border access to genomic and related
-          phenotypic datasets across Europe
-        </li>
-        <li>
-          To advance understanding of genomics for more precise and faster
-          clinical decision making, diagnostics, treatments systems, and to
-          benefit the overall economy
-        </li>
-        <li>
-          To align with the development under the European Health Data Space
-          (EHDS)
-        </li>
-        <li>
-          To Facilitate research, policy-making, and healthcare improvements
-        </li>
-        <li>
-          To maintain awareness, acceptance and trust in the main groups of
-          stakeholders, notably European citizens, data holders, healthcare
-          professionals, researchers and public health authorities
-        </li>
-      </ul> */}
       </p>
       <PageSubHeading className="my-4">Ontwikkeling</PageSubHeading>
       <p>
@@ -90,6 +66,81 @@ const AboutPage: React.FC = () => {
       <p className="mt-4">
         GDI project receives funding from the European Union&lsquo;s Digital
         Europe Programme under grant agreement number 101081813.
+      </p>
+
+      <PageSubHeading className="my-4">Betaversie</PageSubHeading>
+      <p>
+        U kunt alvast kennis maken met de betaversie van de nationale
+        gezondheidsdata catalogus. De nationale catalogus waarin u alle
+        gezondheidsdata kunt gaan vinden voor onderzoek, beleid en innovatie.
+        Doel is om zowel Real World Data uit zorgsystemen, als overige
+        gezondheidsdata, van bijvoorbeeld prospectieve onderzoeken van uw
+        collegae, vindbaar te maken in de nationale catalogus.
+      </p>
+      <p>
+        Momenteel kunt u een beperkte hoeveelheid datasets vinden met een
+        beperkte set aan variabelen waarop u deze zou kunnen filteren. Het plan
+        is om:
+      </p>
+      <ul className="list-inside list-disc">
+        <li>De gebruikersinterface te optimaliseren</li>
+        <li>
+          De hoeveelheid te vinden datasets, die te filteren zijn op de huidige
+          beperkte set variabelen, uit te breiden
+        </li>
+        <li>
+          De set gestandaardiseerde variabelen (metadata) uit te breiden,
+          waarmee datasets te onderscheiden zijn.
+        </li>
+      </ul>
+      <p>
+        We zijn erg nieuwsgierig wat u van de betaversie vindt. Kunt u ons
+        helpen om de nationale gezondheidsdata catalogus beter te maken door ons
+        te voorzien van feedback via het e-mailadres{" "}
+        <a
+          href="mailto:servicedesk@health-ri.nl"
+          className="text-blue-500 hover:underline"
+        >
+          servicedesk@health-ri.nl
+        </a>
+        , met kenmerk “#Feedback_Catalogus”. U kunt dit e-mailadres ook
+        gebruikend om aan te geven dat u op de hoogte gehouden wenst te woorden
+        met kenmerk “#Info_Catalogus”.
+      </p>
+
+      <PageSubHeading className="my-4">Achtergrondinformatie</PageSubHeading>
+      <p>
+        Momenteel is de catalogus in ontwikkeling. In deze betaversie worden
+        pilots uitgevoerd, om metadata over datasets op te nemen volgens een
+        minimale metadataset, gebaseerd op de in Europese portalen gehanteerde
+        standaard DCAT AP. Deze DCAT AP standaard is in ontwikkeling, en veel
+        aspecten zijn nog niet eenduidig beschreven. Dit geeft ruimte voor
+        interpretatieverschillen aan beide zijden, waardoor veel afstemming
+        nodig is om metadata in de juiste vorm aan de catalogus aan te bieden.
+        Hierdoor ziet u momenteel nog geen, of een zeer beperkt aantal datasets
+        in de catalogus.
+      </p>
+      <p>
+        Tijdens de pilots wordt geschreven aan een eerste versie van informatie
+        en handleidingen om metadata over datasets (in de brede zin van het
+        woord, dus ook over samples, beelden, etc.) in de nationale
+        gezondheidsdata catalogus te plaatsen. Een eerste versie van deze
+        documentatie is in consultatie, zie:{" "}
+        <a
+          href="https://health-ri.atlassian.net/wiki/spaces/HWV3/pages/414126205/Onboarding+van+metadata+op+de+Nationale+Gezondheidsdata+Catalogus"
+          className="text-blue-500 hover:underline"
+        >
+          Onboarding van metadata op de Nationale Gezondheidsdata Catalogus
+        </a>
+        .
+      </p>
+
+      <PageSubHeading className="my-4">Beoogde doorontwikkeling</PageSubHeading>
+      <p>
+        In de vervolgontwikkeling zal op basis van Health-DCAT de set aan
+        metadata verder worden uitgebreid. Deze standaard wordt in september
+        2024 gepubliceerd en zal leidend zijn voor de Health-Ring die bepaald
+        wordt om gezondheidsdata op te nemen in de nationale catalogus.
       </p>
     </div>
   );

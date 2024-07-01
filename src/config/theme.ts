b5281aa9a3e3cdf1ecd4f3a18d667312a4cd59e9
zia alborzi <zia.alborzi@lnds.lu>
2024-07-01 10:18:30 +0200

    set default values for fonts and colors

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -5,17 +5,17 @@ import 'dotenv/config';
 
 export const themeConfig = {
   colors: {
-    primary: process.env.NEXT_PUBLIC_COLOR_PRIMARY,
-    secondary: process.env.NEXT_PUBLIC_COLOR_SECONDARY,
-    info: process.env.NEXT_PUBLIC_COLOR_INFO,
-    warning: process.env.NEXT_PUBLIC_COLOR_WARNING,
-    'hover-color': process.env.NEXT_PUBLIC_COLOR_HOVER_COLOR,
-    disclaimer: process.env.NEXT_PUBLIC_disclaimer,
-    surface: process.env.NEXT_PUBLIC_COLOR_SURFACE,
+    primary: process.env.NEXT_PUBLIC_COLOR_PRIMARY || 'var(--color-primary)',
+    secondary: process.env.NEXT_PUBLIC_COLOR_SECONDARY || 'var(--color-secondary)',
+    info: process.env.NEXT_PUBLIC_COLOR_INFO || 'var(--color-info)',
+    warning: process.env.NEXT_PUBLIC_COLOR_WARNING || 'var(--color-warning)',
+    'hover-color': process.env.NEXT_PUBLIC_COLOR_HOVER_COLOR || 'var(--color-hover)',
+    disclaimer: process.env.NEXT_PUBLIC_COLOR_DISCLAIMER || 'var(--color-disclaimer)',
+    surface: process.env.NEXT_PUBLIC_COLOR_SURFACE || 'var(--color-surface)',
   },
   fonts: {
-    light: process.env.NEXT_PUBLIC_SELECTED_FONT_LIGHT,
-    medium: process.env.NEXT_PUBLIC_SELECTED_FONT_MEDIUM,
-    sans: process.env.NEXT_PUBLIC_SELECTED_FONT_SANS,
+    light: process.env.NEXT_PUBLIC_SELECTED_FONT_LIGHT || 'satoshi',
+    medium: process.env.NEXT_PUBLIC_SELECTED_FONT_MEDIUM || 'expose',
+    sans: process.env.NEXT_PUBLIC_SELECTED_FONT_SANS || 'tabular',
   },
 };
